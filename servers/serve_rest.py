99534cc414bd6ae108ae21adee7353b57e4d4e2c
nikola-jovisic <nikolajovisic4@gmail.com>
2024-01-22 12:47:57 +0100

    rest-server: fix path

--- servers/serve_rest.py
+++ servers/serve_rest.py
@@ -4,10 +4,11 @@ from flask import Flask, abort, send_from_directory
 
 app = Flask(__name__)
 
-
+script_dir = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(script_dir, "data")
 @app.route("/<id>", methods=["GET"])
 def download_file(id):
-    directory = f"data/{id}"
+    directory = os.path.join(data_path, id)
     filename = f"{id}.png"
     if os.path.isfile(os.path.join(directory, filename)):
         return send_from_directory(directory, filename, as_attachment=True)
@@ -16,4 +17,4 @@ def download_file(id):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
