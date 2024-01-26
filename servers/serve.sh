5a3c49ad255e712783c1aaf43b6b42052524bd0c
nikola-jovisic <nikolajovisic4@gmail.com>
2024-01-26 16:05:33 +0100

    serve.sh adapted for different configurations

--- servers/serve.sh
+++ servers/serve.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+config="dev"
+
+if [ -n "$1" ]; then
+    param1="$1"
+fi
+
+cp ./configs/"${config}.json" ./config.json
+cp ./configs/"${config}.json" ./mamo-front/src/config.json
+
 python servers/serve_dcm.py &
 python servers/serve_rest.py
