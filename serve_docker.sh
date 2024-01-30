7fd94a0056bfe73a554013d994ae844db573a6ac
Nikola Jovišić <jovisic@bio4.institutonline.ai>
2024-01-30 15:08:56 +0000

    fix port mapping

---
+++ serve_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+config="prod"
+config_file="servers/configs/${config}.json"
+
+rest_port=$(jq '.rest_port' $config_file)
+# dcm_port=$(jq '.dcm_port' $config_file)
+rest_port_inside=$(jq '.rest_port_inside' $config_file)
+# dcm_port_inside=$(jq '.dcm_port_inside' $config_file)
+
+docker build . -t aimamo
+
+docker kill aim
+docker rm aim
+# docker run --name aim -p ${rest_port}:${rest_port_inside} -p ${dcm_port}:${dcm_port_inside} aimamo
+docker run --name aim -p ${rest_port}:${rest_port_inside} aimamo
--- servers/serve.sh
+++ servers/serve.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-config="dev"
-
-if [ -n "$1" ]; then
-    param1="$1"
-fi
-
-cp ./configs/"${config}.json" ./config.json
-cp ./configs/"${config}.json" ./mamo-front/src/config.json
-
-python servers/serve_dcm.py &
-python servers/serve_rest.py
+#python serve_dcm.py &
+python serve_rest.py
