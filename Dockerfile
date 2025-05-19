47483edb25936b8dfb557957f7aea97cf7716716
Nikola Jovišić <nikolajovisic4@gmail.com>
2025-05-19 15:11:04 +0200

    prototype for june2025 demo

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,9 @@ RUN pip install -r /servers/requirements.txt
 RUN apt-get update && apt-get install ffmpeg libsm6 libxext6  -y #required for open-cv
 RUN yes | apt install npm
 COPY . .
-COPY servers/configs/prod.json /servers/config.json
-COPY servers/configs/prod.json /servers/mamo-front/src/config.json
+COPY servers/config.json /servers/mamo-front/src/config.json
 WORKDIR /servers/mamo-front
 RUN npm install
 RUN npm run build
 WORKDIR /servers
-RUN chmod +x serve.sh
-CMD ["./serve.sh"]
+CMD ["python", "serve_rest.py"]
--- serve_docker.sh
+++ serve_docker.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-config="prod"
-config_file="servers/configs/${config}.json"
+config_file="servers/config.json"
 
-rest_port=$(jq '.rest_port' $config_file)
-dcm_port=$(jq '.dcm_port' $config_file)
-rest_port_inside=$(jq '.rest_port_inside' $config_file)
-dcm_port_inside=$(jq '.dcm_port_inside' $config_file)
+external_port=$(jq '.external_port' $config_file)
+internal_port=$(jq '.internal_port' $config_file)
 
 docker build . -t aimamo
 
 docker kill aim
 docker rm aim
-docker run --name aim -p ${rest_port}:${rest_port_inside} -p ${dcm_port}:${dcm_port_inside} aimamo
-
-
+docker run --name aim -p ${external_port}:${internal_port} aimamo
